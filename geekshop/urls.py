e28fd0ca60666c39e874c8257480d52d07a8501a
Dmitr <gaastra104@mail.ru>
2021-04-25 21:17:00 +0800

    #7

--- geekshop/urls.py
+++ geekshop/urls.py
@@ -1,7 +1,7 @@
 from django.conf import settings
 from django.conf.urls.static import static
 from django.contrib import admin
-from django.urls import path, include
+from django.urls import path, include, re_path
 from mainapp import views as mainapp
 
 urlpatterns = [
